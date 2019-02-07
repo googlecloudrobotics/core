648e7c7ed35f37d4bc47f7394fb329d4103e8601
Steffen Kopecki <skopecki@google.com>
2019-02-07 15:20:34 +0100

    Add unit test for en-/disabling app management
    This was done in the integration test but without verifying that the
    core functionality remains when the app layer is disabled. In a
    follow-up CL the app management test will be removed from the
    integration test.
    
    Change-Id: I9a2a5ad16abb8cca68b56b9997d020aa4b885688
    GitOrigin-RevId: 8ccb3e5da7e9f99699d04c9e0440e4eaf91ea1a2

---
+++ src/app_charts/base/app_management_test.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+HELM="external/kubernetes_helm/helm template"
+CLOUD_BASE="src/app_charts/base/base-cloud-0.0.1.tgz"
+ROBOT_BASE="src/app_charts/base/base-robot-0.0.1.tgz"
+
+function test_failed() {
+  echo "TEST FAILED: $1"
+  exit 1
+}
+
+function test_passed() {
+  echo "TEST PASSED: $1"
+}
+
+function expect_app_installed() {
+  local command="$1"
+  local application="$2"
+  local template
+  if ! template=$(${command}); then
+    test_failed "\"${command}\" failed"
+  fi
+  if [[ "${template}" != *"app: ${application}"* ]]; then
+    echo "TEMPLATE: ${template}"
+    test_failed "expected \"${application}\" to be installed in template created by \"${command}\""
+  fi
+  test_passed "application \"${application}\" is included in template created by \"${command}\""
+}
+
+function expect_app_not_installed() {
+  local command="$1"
+  local application="$2"
+  local template
+  if ! template=$(${command}); then
+    echo "TEMPLATE: ${template}"
+    test_failed "\"${command}\" failed"
+  fi
+  if [[ "${template}" == *"app: ${application}"* ]]; then
+    test_failed "did not expected \"${application}\" to be installed in template created by \"${command}\""
+  fi
+  test_passed "application \"${application}\" is not included in template created by \"${command}\""
+}
+
+expect_app_installed "${HELM} ${CLOUD_BASE} --set-string app_management=true" "cloud-master"
+expect_app_not_installed "${HELM} ${CLOUD_BASE} --set-string app_management=false" "cloud-master"
+
+expect_app_installed "${HELM} ${ROBOT_BASE} --set-string app_management=true" "robot-master"
+expect_app_not_installed "${HELM} ${ROBOT_BASE} --set-string app_management=false" "robot-master"
