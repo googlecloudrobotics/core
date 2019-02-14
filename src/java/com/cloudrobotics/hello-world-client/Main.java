db208404da01ae397d3a524506df0f95c44db163
Steve Wolter <swolter@google.com>
2019-02-14 15:32:11 +0100

    Add coverage the Hello World client example.
    This is now free-standing and also covers edge cases like timestamps.
    
    b/124437335
    
    Change-Id: I9496504b638ae6af3885a20aeedf18e267aca683
    GitOrigin-RevId: b7c8694f2f920371ee228bb53a6e499e3b727783

--- src/java/com/cloudrobotics/hello-world-client/Main.java
+++ src/java/com/cloudrobotics/hello-world-client/Main.java
@@ -14,8 +14,12 @@ final class Main {
     K8sHelloWorldGrpc.K8sHelloWorldBlockingStub stub =
         K8sHelloWorldGrpc.newBlockingStub(
             ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build());
+    Service.CreateHelloWorldRequest.Builder req = Service.CreateHelloWorldRequest.newBuilder();
+    req.getObjectBuilder().getMetadataBuilder().setName("foo");
+    stub.create(req.build());
     Service.HelloWorld world =
         stub.get(Service.GetHelloWorldRequest.newBuilder().setName("foo").build());
     System.out.println(world.getMetadata().getResourceVersion());
+    stub.delete(Service.DeleteHelloWorldRequest.newBuilder().setName("foo").build());
   }
 }
