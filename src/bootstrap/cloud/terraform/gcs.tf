db590ec523afeaae2dfdd67833d065e7568f1cf6
Rodrigo Queiro <rodrigoq@google.com>
2019-02-21 17:21:48 +0100

    Make entire -robot bucket public
    The workaround for
    https://github.com/terraform-providers/terraform-provider-google/issues/2213
    appears to be unreliable, so we just make the entire bucket public
    instead.
    
    Fixes b/125299150.
    
    Change-Id: I42f4c6fe067ce069100a144f7a04a5d6e038ad81
    GitOrigin-RevId: b0b52e56a4604ca504ac31428b86283eee2c8797

--- src/bootstrap/cloud/terraform/gcs.tf
+++ src/bootstrap/cloud/terraform/gcs.tf
@@ -6,18 +6,20 @@ resource "google_storage_bucket" "robot" {
   depends_on    = ["google_project_service.compute"]
 }
 
+# Make the bucket, and its contents, public. Making the individual objects
+# public is hard because of
+# https://github.com/terraform-providers/terraform-provider-google/issues/2213
+resource "google_storage_bucket_iam_member" "robot-acl" {
+  bucket = "${google_storage_bucket.robot.name}"
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
+
 resource "google_storage_bucket_object" "setup_robot_image_reference" {
   name          = "setup_robot_image_reference.txt"
   source        = "../../../../bazel-genfiles/src/bootstrap/robot/setup_robot_image_reference.txt"
   bucket        = "${google_storage_bucket.robot.name}"
   cache_control = "private, max-age=0, no-transform"
-}
-
-resource "google_storage_object_access_control" "setup_robot_image_reference_ac" {
-  object = "${google_storage_bucket_object.setup_robot_image_reference.output_name}"
-  bucket = "${google_storage_bucket.robot.name}"
-  role   = "READER"
-  entity = "allUsers"
 }
 
 resource "google_storage_bucket_object" "setup_robot" {
@@ -27,25 +29,11 @@ resource "google_storage_bucket_object" "setup_robot" {
   cache_control = "private, max-age=0, no-transform"
 }
 
-resource "google_storage_object_access_control" "setup_robot_ac" {
-  object = "${google_storage_bucket_object.setup_robot.output_name}"
-  bucket = "${google_storage_bucket.robot.name}"
-  role   = "READER"
-  entity = "allUsers"
-}
-
 resource "google_storage_bucket_object" "install_k8s_on_robot" {
   name          = "install_k8s_on_robot.sh"
   source        = "../../robot/install_k8s_on_robot.sh"
   bucket        = "${google_storage_bucket.robot.name}"
   cache_control = "private, max-age=0, no-transform"
-}
-
-resource "google_storage_object_access_control" "install_k8s_on_robot_ac" {
-  object = "${google_storage_bucket_object.install_k8s_on_robot.output_name}"
-  bucket = "${google_storage_bucket.robot.name}"
-  role   = "READER"
-  entity = "allUsers"
 }
 
 resource "google_storage_bucket" "tools" {
