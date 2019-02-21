1034c0675a1bc2b56d308acbb4c560076581c779
Steffen Kopecki <skopecki@google.com>
2019-02-21 10:27:40 +0100

    Install latest release candidate in run-install.sh
    Also fixes a naming difference between infrastructure/ci/integration_test.sh
    and the binary. http://b/125155665
    
    Change-Id: I72e4e9dbcee4e609188cbbef575ae4a8f89a01cd
    GitOrigin-RevId: a50458ab3c98289a68b3fa57daeddcd51875a897

--- src/bootstrap/cloud/run-install.sh
+++ src/bootstrap/cloud/run-install.sh
@@ -18,7 +18,7 @@ set -o pipefail -o errexit
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Usage: ./install.sh [cloud-storage-bucket-url] [versioned-tarball]
+# Usage: ./run-install.sh [cloud-storage-bucket-url] [versioned-tarball]
 if [[ $# -ge 2 ]]; then
   GCP_BUCKET=${1%/}
   TARBALL=$2
@@ -32,10 +32,9 @@ elif [[ $# -eq 1 ]]; then
 fi
 
 GCP_BUCKET=${GCP_BUCKET:-"https://storage.googleapis.com/cloud-robotics-releases"}
-# TODO(skopecki): Download the latest, stable version by default.
-TARBALL=${TARBALL:-"cloud-robotics-core.tar.gz"}
+TARBALL=${TARBALL:-"crc-rc-latest.tar.gz"}
 
 echo "Downloading ${GCP_BUCKET}/${TARBALL}"
-curl "${GCP_BUCKET}/${TARBALL}" | tar xvz
+curl --silent --show-error --fail "${GCP_BUCKET}/${TARBALL}" | tar xz
 
 (cd ${DIR}/cloud-robotics-core && ./deploy.sh create)
