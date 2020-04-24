ae3f36270fe4464cf6130a06a0e33dfcc0273e58
Rodrigo Queiro <rodrigoq@google.com>
2020-04-24 15:16:16 +0200

    Switch cluster to e2-standard-4 VM type.
    This should reduce cost with minimal effect on performance.
    
    This also uses the "separately managed node pool" pattern recommended at
    https://www.terraform.io/docs/providers/google/r/container_node_pool.html.
    I hope that will allow us to make future node pool changes without
    destroying the cluster.
    
    Change-Id: Icbee191cff82dcad7e1ad3935e66295c462201f4
    GitOrigin-RevId: e65460691a310d0dc610c70d3b9cae78cc574e4e

--- src/bootstrap/cloud/terraform/cluster.tf
+++ src/bootstrap/cloud/terraform/cluster.tf
@@ -5,44 +5,52 @@ resource "google_container_cluster" "cloud-robotics" {
   enable_legacy_abac = true
   depends_on         = ["google_project_service.container"]
 
-  node_pool {
-    initial_node_count = 2
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
 
-    autoscaling = {
-      min_node_count = 2
-      max_node_count = 10
-    }
-
-    node_config {
-      machine_type = "n1-standard-4"
-
-      oauth_scopes = [
-        "https://www.googleapis.com/auth/bigquery",
-        "https://www.googleapis.com/auth/cloud-platform",
-        "https://www.googleapis.com/auth/cloud-platform.read-only",
-        "https://www.googleapis.com/auth/cloud.useraccounts",
-        "https://www.googleapis.com/auth/compute",
-        "https://www.googleapis.com/auth/datastore",
-        "https://www.googleapis.com/auth/devstorage.full_control",
-        "https://www.googleapis.com/auth/logging.write",
-        "https://www.googleapis.com/auth/monitoring",
-        "https://www.googleapis.com/auth/monitoring.write",
-        "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
-        "https://www.googleapis.com/auth/pubsub",
-        "https://www.googleapis.com/auth/servicecontrol",
-        "https://www.googleapis.com/auth/trace.append",
-        "https://www.googleapis.com/auth/userinfo.email",
-
-        # taskqueue is unused: add/remove it to trigger cluster recreation.
-        "https://www.googleapis.com/auth/taskqueue",
-      ]
-    }
-  }
+  initial_node_count = 1
 
   timeouts {
     create = "1h"
     update = "1h"
     delete = "1h"
+  }
+}
+
+resource "google_container_node_pool" "cloud-robotics" {
+  name     = "cloud-robotics"
+  location = "${var.zone}"
+  cluster  = "${google_container_cluster.cloud-robotics.name}"
+
+  initial_node_count = 2
+
+  autoscaling = {
+    min_node_count = 2
+    max_node_count = 10
+  }
+
+  node_config {
+    machine_type = "e2-standard-4"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/bigquery",
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/cloud-platform.read-only",
+      "https://www.googleapis.com/auth/cloud.useraccounts",
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/datastore",
+      "https://www.googleapis.com/auth/devstorage.full_control",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
+      "https://www.googleapis.com/auth/pubsub",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ]
   }
 }
 
