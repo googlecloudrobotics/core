1fc20629420973482524cb38c5bcf4ba0920afa9
Sam Khan <sammyk@google.com>
2020-09-30 15:38:58 +0200

    Remove unused scope from setup-robot-vm.sh.
    This was causing an alert to be triggered about overly permissive roles.
    
    Change-Id: I28fb9949df65444b9551213f9ae81e1410e11f5f
    GitOrigin-RevId: dfecd5427ec534e4e4346026410b29d190e073ae

--- scripts/setup-robot-vm.sh
+++ scripts/setup-robot-vm.sh
@@ -36,8 +36,7 @@ gcloud --project=$GCP_PROJECT_ID compute instances create ${ROBOT_NAME} \
   --image-family ubuntu-1604-lts \
   --image-project ubuntu-os-cloud \
   --boot-disk-size=200GB \
-  --machine-type=e2-standard-2 \
-  --scopes=cloud-platform
+  --machine-type=e2-standard-2
 gcloud --project=$GCP_PROJECT_ID compute instances add-tags ${ROBOT_NAME}  \
   --tags=kokoro-ssh
 
