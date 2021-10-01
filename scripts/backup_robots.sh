550c52852286b9f1318ad00084c3553fbe015c28
Stefan Sauer <ensonic@google.com>
2021-10-01 11:29:40 +0200

    Add a script to backup robot custom-resources.
    The script can be used to migrate the custom-resources to a newer api-version.
    
    Change-Id: Id84b484ffea6d34ecc5ad79eb487e5ce42944a89
    GitOrigin-RevId: 2fee6dcf128795d54f29a281b264301d3bc7b255

---
+++ scripts/backup_robots.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Copyright 2021 The Cloud Robotics Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# see https://github.com/kubernetes/kubernetes/issues/90066#issuecomment-780236185 for hiding managed-fields
+# another tool: https://github.com/itaysk/kubectl-neat
+
+if ! hash yq; then
+  echo "This script needs yq (pip3 install yq)"
+  exit
+fi
+
+KUBE_CONTEXT=${KUBE_CONTEXT:-minikube}
+LAST_APPLIED="kubectl.kubernetes.io/last-applied-configuration"
+
+function kc {
+  kubectl --context="${KUBE_CONTEXT}" "$@"
+}
+
+for r in $(kc get robots -o name); do
+  yaml=$(kc get $r -o yaml)
+  if echo ${yaml} | grep -q "${LAST_APPLIED}"; then
+    echo "${yaml}" | \
+      yq 2>/dev/null -ry ".metadata.annotations[\"${LAST_APPLIED}\"]" -
+   else
+     echo "${yaml}" | \
+       yq -ry 'del(.metadata.creationTimestamp,.metadata.generation,.metadata.managedFields,.metadata.resourceVersion,.metadata.selfLink,.metadata.uid,.status)' -
+   fi
+  echo "---"
+done
+
