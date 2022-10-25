44bc83d422c092b7099077b1e157a3106e0fc661
Carsten Schiemann <cschiemann@google.com>
2022-10-25 16:45:55 +0200

    chartassignment: split the validator code into a separate file
    Change-Id: If2ec4f8dc8068a40346556f9969b5bd0eb543ff8
    GitOrigin-RevId: 09d4947710e6436229adcae58facc6fa40d0975e

--- src/go/pkg/controller/chartassignment/controller.go
+++ src/go/pkg/controller/chartassignment/controller.go
@@ -19,8 +19,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
-	"strings"
 	"time"
 
 	apps "github.com/googlecloudrobotics/core/src/go/pkg/apis/apps/v1alpha1"
@@ -28,10 +26,7 @@ import (
 	"github.com/pkg/errors"
 	core "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/api/validation"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/client-go/tools/record"
 	"k8s.io/client-go/util/workqueue"
 	kclient "sigs.k8s.io/controller-runtime/pkg/client"
@@ -41,7 +36,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
-	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 const (
@@ -501,86 +495,4 @@ func setCondition(as *apps.ChartAssignment, t apps.ChartAssignmentConditionType,
 		Status:             v,
 		Message:            msg,
 	})
-}
-
-// NewValidationWebhook returns a new webhook that validates ChartAssignments.
-func NewValidationWebhook(mgr manager.Manager) *admission.Webhook {
-	return &admission.Webhook{Handler: newChartAssignmentValidator(mgr.GetScheme())}
-}
-
-// NewValidationWebhookForEdgeCluster returns a webhook that checks
-// ChartAssignments are valid and apply to a cluster with the given name.
-func NewValidationWebhookForEdgeCluster(mgr manager.Manager, clusterName string) *admission.Webhook {
-	v := newChartAssignmentValidator(mgr.GetScheme())
-	v.clusterName = clusterName
-	return &admission.Webhook{Handler: v}
-}
-
-// chartAssignmentValidator implements a validation webhook.
-type chartAssignmentValidator struct {
-	decoder     runtime.Decoder
-	clusterName string
-}
-
-func newChartAssignmentValidator(sc *runtime.Scheme) *chartAssignmentValidator {
-	return &chartAssignmentValidator{
-		decoder: serializer.NewCodecFactory(sc).UniversalDeserializer(),
-	}
-}
-
-func (v *chartAssignmentValidator) Handle(_ context.Context, req admission.Request) admission.Response {
-	cur := &apps.ChartAssignment{}
-	old := &apps.ChartAssignment{}
-
-	if err := runtime.DecodeInto(v.decoder, req.AdmissionRequest.Object.Raw, cur); err != nil {
-		return admission.Errored(http.StatusBadRequest, err)
-	}
-	if len(req.AdmissionRequest.OldObject.Raw) > 0 {
-		if err := runtime.DecodeInto(v.decoder, req.AdmissionRequest.OldObject.Raw, old); err != nil {
-			return admission.Errored(http.StatusBadRequest, err)
-		}
-	} else {
-		old = nil
-	}
-	if err := v.validate(cur, old); err != nil {
-		return admission.Denied(err.Error())
-	}
-	return admission.Allowed("")
-}
-
-func (v *chartAssignmentValidator) validate(cur, old *apps.ChartAssignment) error {
-	if cur.Spec.ClusterName == "" {
-		return fmt.Errorf("cluster name missing")
-	}
-	if v.clusterName != "" && cur.Spec.ClusterName != v.clusterName {
-		return fmt.Errorf("invalid cluster name %q, expected %q", cur.Spec.ClusterName, v.clusterName)
-	}
-	if cur.Spec.NamespaceName == "" {
-		return fmt.Errorf("namespace name missing")
-	}
-	errs := validation.ValidateNamespaceName(cur.Spec.NamespaceName, false)
-	if len(errs) > 0 {
-		return fmt.Errorf("invalid namespace name %q: %s", cur.Spec.NamespaceName, strings.Join(errs, ", "))
-	}
-	errs = validation.ValidateClusterName(cur.Spec.ClusterName, false)
-	if len(errs) > 0 {
-		return fmt.Errorf("invalid cluster name %q: %s", cur.Spec.ClusterName, strings.Join(errs, ", "))
-	}
-	if old != nil {
-		if cur.Spec.NamespaceName != old.Spec.NamespaceName {
-			return fmt.Errorf("target namespace name must not be changed")
-		}
-		if cur.Spec.ClusterName != old.Spec.ClusterName {
-			return fmt.Errorf("target cluster name must not be changed")
-		}
-	}
-	c := cur.Spec.Chart
-	if c.Inline != "" {
-		if c.Repository != "" || c.Name != "" || c.Version != "" {
-			return fmt.Errorf("chart repository, name, and version must be empty for inline charts")
-		}
-	} else if c.Repository == "" || c.Name == "" || c.Version == "" {
-		return fmt.Errorf("non-inline chart must be fully specified")
-	}
-	return nil
 }
