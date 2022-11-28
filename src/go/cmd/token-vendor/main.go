410b8df45e457f4be1e27c72aa9ae0779c88eaeb
Christian Sieber <csieber@google.com>
2022-11-28 09:09:34 +0000

    Fixed missing auth init import.
    Fixes b/260404648
    
    Change-Id: I16f287a9f5cbffcc1c0dd28ee761946da3956edb
    GitOrigin-RevId: f5de1bcb6c51ce6aa7621bf06c37d0139c32225c

--- src/go/cmd/token-vendor/main.go
+++ src/go/cmd/token-vendor/main.go
@@ -24,6 +24,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
 
 	"github.com/googlecloudrobotics/core/src/go/cmd/token-vendor/api"
