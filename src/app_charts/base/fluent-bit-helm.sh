a767b7a75dc69b82398183c673f7debfb6a868c1
Alex Faxå <alexanderfaxa@google.com>
2023-02-22 17:14:24 +0100

    fluent-bit: add tolerations and update to 2.0.9 (#99)

--- src/app_charts/base/fluent-bit-helm.sh
+++ src/app_charts/base/fluent-bit-helm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # needs at least helm v3.5.0
 OUTPUT=./robot/fluent-bit.yaml
-TEMPLATE_VERSION=0.20.5
+TEMPLATE_VERSION=0.24.0
 helm repo add fluent https://fluent.github.io/helm-charts
 helm repo update fluent
 helm template fluent-bit fluent/fluent-bit --version ${TEMPLATE_VERSION} -f fluent-bit-values.yaml --skip-tests > ${OUTPUT}
