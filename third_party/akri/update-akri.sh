59b7f9050a56db962f7d1d0b2ef17d959e961664
ajinkyajain <ajinkyajain@google.com>
2023-04-05 19:23:55 +0000

    Updates from feedback

--- third_party/akri/update-akri.sh
+++ third_party/akri/update-akri.sh
@@ -9,8 +9,6 @@ helm pull akri-helm-charts/akri --untar
 cp -r ./akri/crds/* .
 
 # Edit helm-charts
-sed -i 's/v2/v1/' ./akri/Chart.yaml
-
 sed -i 's/.Chart.AppVersion/.Values.akriAppVersion/g' ./akri/templates/agent.yaml
 sed -i '28,92s/^/  /' akri/templates/agent.yaml
 sed -i '98,120s/^/  /' akri/templates/agent.yaml
