48cec7a7b3412baa94729eb30da7363a6467597e
Markus Ongyerth <ongyerth@google.com>
2023-04-25 07:48:40 +0000

    Add regional cluster configuration to terraform
    This allows to run multiple clusters in a cloud robitics cloud project.
    These clusters can be used to have relays in a region closer to the
    user and their robot.
    
    DNS entries will be generated as subdomains of the primary domain.

--- src/bootstrap/cloud/terraform/address.tf
+++ src/bootstrap/cloud/terraform/address.tf
@@ -4,3 +4,11 @@ resource "google_compute_address" "cloud_robotics" {
   region     = var.region
   depends_on = [google_project_service.compute]
 }
+
+resource "google_compute_address" "cloud_robotics_ar" {
+  for_each   = var.additional_regions
+  name       = format("%s-%s", each.key, "ar-cloud-robotics")
+  project    = data.google_project.project.project_id
+  region     = each.value.region
+  depends_on = [google_project_service.compute]
+}
