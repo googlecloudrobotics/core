e849da0a6c9ec9a21d921d3d59c66b5fa065143e
Alex Faxa <alexanderfaxa@google.com>
2023-07-25 09:24:27 +0000

    make sure digest is included in both binary and source versions with the same path

--- .github/ci/common.sh
+++ .github/ci/common.sh
@@ -87,7 +87,8 @@ function release_binary {
       //src/go/cmd/setup-robot:setup-robot.push
 
   # The push scripts depends on binaries in the runfiles.
-  local oldPwd=$(pwd)
+  local oldPwd
+  oldPwd=$(pwd)
   # The tag variable must be called 'TAG', see cloud-robotics/bazel/container_push.bzl
   for t in latest ${DOCKER_TAG}; do
     cd ${oldPwd}/bazel-bin/src/go/cmd/setup-robot/push_setup-robot.push.sh.runfiles/cloud_robotics
