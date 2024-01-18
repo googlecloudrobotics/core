17df140327ecb7799701e07a603eceec308db273
therjak <therjak@gmx.net>
2024-01-18 11:37:55 +0100

    chart-assignment-controller: switch to log/slog (#297)

--- src/go/cmd/chart-assignment-controller/main.go
+++ src/go/cmd/chart-assignment-controller/main.go
@@ -20,13 +20,14 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
 	"os"
 
 	"contrib.go.opencensus.io/exporter/stackdriver"
 	apps "github.com/googlecloudrobotics/core/src/go/pkg/apis/apps/v1alpha1"
 	"github.com/googlecloudrobotics/core/src/go/pkg/controller/chartassignment"
+	"github.com/googlecloudrobotics/ilog"
 	"github.com/pkg/errors"
 	"go.opencensus.io/trace"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -62,13 +63,17 @@ var (
 
 func main() {
 	flag.Parse()
+	logHandler := ilog.NewLogHandler(slog.LevelInfo, os.Stderr)
+	slog.SetDefault(slog.New(logHandler))
+
 	ctx := context.Background()
 	if *stackdriverProjectID != "" && *cloudCluster == false {
 		sd, err := stackdriver.NewExporter(stackdriver.Options{
 			ProjectID: *stackdriverProjectID,
 		})
 		if err != nil {
-			log.Fatalf("Failed to create the Stackdriver exporter: %v", err)
+			slog.Error("Failed to create the Stackdriver exporter", ilog.Err(err))
+			os.Exit(1)
 		}
 		trace.RegisterExporter(sd)
 		trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
@@ -78,24 +83,27 @@ func main() {
 	var clusterName string
 	if *cloudCluster == true {
 		clusterName = "cloud"
-		log.Print("Starting chart-assigment-controller in cloud setup")
+		slog.Info("Starting chart-assignment-controller in cloud setup")
 	} else {
 		clusterName = os.Getenv("ROBOT_NAME")
-		log.Printf("Starting chart-assigment-controller in robot setup with cluster name %s", clusterName)
+		slog.Info("Starting chart-assigment-controller in robot setup", slog.String("Cluster", clusterName))
 		if clusterName == "" {
-			log.Fatalf("expect ROBOT_NAME environment var to be set to an non-empty string")
+			slog.Error("expect ROBOT_NAME environment var to be set to an non-empty string")
+			os.Exit(1)
 		}
 	}
 
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		log.Fatalln(err)
+		slog.Error("Failed to load config", ilog.Err(err))
+		os.Exit(1)
 	}
 	config.QPS = float32(*maxQPS)
 	// The default value of twice the max QPS seems to work well.
 	config.Burst = *maxQPS * 2
 
-	log.Fatal(runController(ctx, config, clusterName))
+	slog.Error("Controller terminated", ilog.Err(runController(ctx, config, clusterName)))
+	os.Exit(1)
 }
 
 func runController(ctx context.Context, cfg *rest.Config, cluster string) error {
