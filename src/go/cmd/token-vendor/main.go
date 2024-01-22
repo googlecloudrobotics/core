7ca50b24a08678d79a61e896e09f85271191761b
therjak <therjak@gmx.net>
2024-01-22 11:50:39 +0100

    token-vendor: switch to log/slog (#294)

--- src/go/cmd/token-vendor/main.go
+++ src/go/cmd/token-vendor/main.go
@@ -18,11 +18,12 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
 	"path"
 	"strings"
 
-	log "github.com/sirupsen/logrus"
 	"k8s.io/client-go/kubernetes"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
@@ -34,6 +35,7 @@ import (
 	"github.com/googlecloudrobotics/core/src/go/cmd/token-vendor/repository/k8s"
 	"github.com/googlecloudrobotics/core/src/go/cmd/token-vendor/repository/memory"
 	"github.com/googlecloudrobotics/core/src/go/cmd/token-vendor/tokensource"
+	"github.com/googlecloudrobotics/ilog"
 )
 
 type scopeFlags []string
@@ -64,7 +66,7 @@ var (
 		"key-store",
 		string(Kubernetes),
 		"Public key repository implementation to use. Options: "+strings.Join(keyStoreOpts, ","))
-	k8sQPS = flag.Int("k8s-qps", 25, "Limit of QPS to the Kubernetes API server.")
+	k8sQPS   = flag.Int("k8s-qps", 25, "Limit of QPS to the Kubernetes API server.")
 	k8sBurst = flag.Int("k8s-burst", 50, "Burst limit of QPS to the Kubernetes API server.")
 	// API options
 	bind     = flag.String("bind", "0.0.0.0", "Address to bind to")
@@ -91,11 +93,12 @@ var (
 func main() {
 	flag.Var(&scopes, "scope", "GCP scopes included in the token given out to robots.")
 	flag.Parse()
+	level := slog.LevelInfo
 	if *verbose {
-		log.SetLevel(log.DebugLevel)
-	} else {
-		log.SetLevel(log.InfoLevel)
+		level = slog.LevelDebug
 	}
+	logHandler := ilog.NewLogHandler(level, os.Stderr)
+	slog.SetDefault(slog.New(logHandler))
 	// init components
 	ctx := context.Background()
 	var rep app.PubKeyRepository
@@ -103,53 +106,64 @@ func main() {
 	if *keyStore == Kubernetes {
 		config, err := rest.InClusterConfig()
 		if err != nil {
-			log.Panic(err)
+			slog.Error("Failed to get config", ilog.Err(err))
+			os.Exit(1)
 		}
 		config.QPS = float32(*k8sQPS)
 		config.Burst = *k8sBurst
 		cs, err := kubernetes.NewForConfig(config)
 		if err != nil {
-			log.Panic(err)
+			slog.Error("Failed to make clientset", ilog.Err(err))
+			os.Exit(1)
 		}
 		rep, err = k8s.NewK8sRepository(ctx, cs, *namespace)
 		if err != nil {
-			log.Panic(err)
+			slog.Error("Failed to make k8s repository client", ilog.Err(err))
+			os.Exit(1)
 		}
 	} else if *keyStore == Memory {
 		rep, err = memory.NewMemoryRepository(ctx)
 		if err != nil {
-			log.Panic(err)
+			slog.Error("Failed to make in-memory repository", ilog.Err(err))
+			os.Exit(1)
 		}
 	} else {
-		log.Panicf("unsupported key store option %q", *keyStore)
+		slog.Error("unsupported key store option", slog.String("Value", *keyStore))
+		os.Exit(1)
 	}
-	log.Infof("Using key store %q", *keyStore)
+	slog.Info("Set up key store", slog.String("KeyStore", *keyStore))
 
 	verifier, err := oauth.NewTokenVerifier(ctx, &http.Client{}, *project)
 	if err != nil {
-		log.Panic(err)
+		slog.Error("Failed to make verifier", ilog.Err(err))
+		os.Exit(1)
 	}
 	ts, err := tokensource.NewGCPTokenSource(ctx, nil, *project, *robotName, scopes)
 	if err != nil {
-		log.Panic(err)
+		slog.Error("Failed to make token source", ilog.Err(err))
+		os.Exit(1)
 	}
 	tv, err := app.NewTokenVendor(ctx, rep, verifier, ts, *acceptedAudience)
 	if err != nil {
-		log.Panic(err)
+		slog.Error("Failed to make token vendor", ilog.Err(err))
+		os.Exit(1)
 	}
 
 	// register API endpoints
 	if err := api.Register(); err != nil {
-		log.Panic(err)
+		slog.Error("Failed to register root endpoints", ilog.Err(err))
+		os.Exit(1)
 	}
 	if err := apiv1.Register(tv, path.Join(*basePath, "v1")); err != nil {
-		log.Panic(err)
+		slog.Error("Failed to register v1 endpoints", ilog.Err(err))
+		os.Exit(1)
 	}
 
 	// serve API
 	addr := fmt.Sprintf("%s:%d", *bind, *port)
 	err = api.SetupAndServe(addr)
 	if err != nil {
-		log.Panicf("failed to listen on %s: %v", addr, err)
+		slog.Error("Failed to listen", slog.String("IP", addr), ilog.Err(err))
+		os.Exit(1)
 	}
 }
