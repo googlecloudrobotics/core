b0c6e8a6cbde1bbee3c070f0a4dbb2ebcc09e1d8
Alex Faxa <alexanderfaxa@google.com>
2024-02-02 14:59:38 +0000

    smaller image

--- .github/ci/deploy_navtest.sh
+++ .github/ci/deploy_navtest.sh
@@ -6,13 +6,11 @@ source "${DIR}/common.sh"
 PROJECT_DIR="${DIR}/deployments/robco-navtest"
 source "${PROJECT_DIR}/config.sh"
 
-# gcloud auth configure-docker --quiet
-
 # TODO(skopecki) These variables should be declared in the run-install.sh and removed from this script.
 export BUCKET_URI="https://storage.googleapis.com/robco-ci-binary-builds"
 export SOURCE_CONTAINER_REGISTRY="gcr.io/robco-team"
 
 # Deploy the binary release that was pushed by the last successful integration test.
-curl --silent --show-error "${BUCKET_URI}/run-install.sh" \
+curl --silent --show-error --fail "${BUCKET_URI}/run-install.sh" \
     | bash -x -s -- ${GCP_PROJECT_ID}
 
