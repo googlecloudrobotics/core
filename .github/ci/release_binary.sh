2bd9955fc6cb7bb4af1205b063996a09307b0de9
Alex Faxa <alexanderfaxa@google.com>
2024-02-05 10:37:40 +0000

    Prepare for review

--- .github/ci/release_binary.sh
+++ .github/ci/release_binary.sh
@@ -10,8 +10,7 @@ gcloud auth configure-docker --quiet
 
 # Set defaults used for the release, they can only be overriden when testing
 # manually.
-GCP_BUCKET=${GCP_BUCKET:-"alexanderfaxa-test"}
-# GCP_BUCKET=${GCP_BUCKET:-"cloud-robotics-releases"}
+GCP_BUCKET=${GCP_BUCKET:-"cloud-robotics-releases"}
 VERSION=${VERSION:-"0.1.0"}
 SHA=$(git rev-parse --short "$FULL_SHA")
 RELEASE_NAME="v$VERSION-$SHA"
@@ -19,20 +18,20 @@ RELEASE_NAME="v$VERSION-$SHA"
 export TAG="crc-${VERSION}-${SHA}"
 LABELS=${LABELS:-"latest crc-${VERSION}/crc-${VERSION}+latest"}
 
-# # Get the last release. We only create a new release if the main branch has moved since
-# # as trying to re-create an existing release is an error.
-# output=$(curl --fail-with-body -sS \
-#   -H "Accept: application/vnd.github+json" \
-#   -H "Authorization: token $GITHUB_TOKEN" \
-#   https://api.github.com/repos/$REPO/releases/latest)
-# PREVIOUS_RELEASE_NAME="$(jq -r '.tag_name'   <<< $output)"
+# Get the last release. We only create a new release if the main branch has moved since
+# as trying to re-create an existing release is an error.
+output=$(curl --fail-with-body -sS \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/$REPO/releases/latest)
+PREVIOUS_RELEASE_NAME="$(jq -r '.tag_name'   <<< $output)"
 
-# if [ "$RELEASE_NAME" = "$PREVIOUS_RELEASE_NAME" ]; then
-#     echo "Release $RELEASE_NAME already exists. Nothing more to do."
-#     exit 0
-# else
-#     echo "Previous release is $PREVIOUS_RELEASE_NAME"
-# fi
+if [ "$RELEASE_NAME" = "$PREVIOUS_RELEASE_NAME" ]; then
+    echo "Release $RELEASE_NAME already exists. Nothing more to do."
+    exit 0
+else
+    echo "Previous release is $PREVIOUS_RELEASE_NAME"
+fi
 
 CLOUD_ROBOTICS_CONTAINER_REGISTRY="gcr.io/cloud-robotics-releases"
 # DOCKER_TAG is a global variable that is used in release_binary.
@@ -40,27 +39,27 @@ DOCKER_TAG=${DOCKER_TAG:-"crc-${VERSION}-${SHA}"}
 
 release_binary "${GCP_BUCKET}" "crc-${VERSION}/crc-${VERSION}+${SHA}" ${LABELS}
 
-# # Generate release notes comparing against the previous release.
-# output=$(curl --fail-with-body -sS \
-#   -X POST \
-#   -H "Accept: application/vnd.github+json" \
-#   -H "Authorization: token $GITHUB_TOKEN" \
-#   https://api.github.com/repos/$REPO/releases/generate-notes \
-#   -d '{"tag_name":"'$RELEASE_NAME'","previous_tag_name":"'$PREVIOUS_RELEASE_NAME'"}')
-# # Code newlines as literal \n and escape double quotes to generate valid JSON.
-# BODY="$(jq -r '.body'   <<< $output | awk '{printf "%s\\n", $0}' | sed 's/"/\\"/g')"
-# echo "Generated release notes for $RELEASE_NAME"
+# Generate release notes comparing against the previous release.
+output=$(curl --fail-with-body -sS \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/$REPO/releases/generate-notes \
+  -d '{"tag_name":"'$RELEASE_NAME'","previous_tag_name":"'$PREVIOUS_RELEASE_NAME'"}')
+# Code newlines as literal \n and escape double quotes to generate valid JSON.
+BODY="$(jq -r '.body'   <<< $output | awk '{printf "%s\\n", $0}' | sed 's/"/\\"/g')"
+echo "Generated release notes for $RELEASE_NAME"
 
-# # Create the release on GitHub.
-# curl --fail-with-body -sS \
-#   -X POST \
-#   -H "Accept: application/vnd.github+json" \
-#   -H "Authorization: token $GITHUB_TOKEN" \
-#   https://api.github.com/repos/$REPO/releases \
-#   --data-binary @- << EOF
-# {
-#   "tag_name": "$RELEASE_NAME",
-#   "name": "$RELEASE_NAME",
-#   "body": "$BODY"
-# }
-# EOF
+# Create the release on GitHub.
+curl --fail-with-body -sS \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/$REPO/releases \
+  --data-binary @- << EOF
+{
+  "tag_name": "$RELEASE_NAME",
+  "name": "$RELEASE_NAME",
+  "body": "$BODY"
+}
+EOF
