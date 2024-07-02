bbf72ec527b43e034e032f1c288fc962d18742bb
Rodrigo Queiro <rodrigoq@google.com>
2024-07-02 10:20:31 +0200

    Add explanatory comments

--- src/bootstrap/cloud/terraform/dns.tf
+++ src/bootstrap/cloud/terraform/dns.tf
@@ -2,6 +2,8 @@ resource "google_dns_managed_zone" "external-dns" {
   name        = "external-dns"
   dns_name    = "${var.domain}."
   count       = var.domain == "" ? 0 : 1
+  # This is used to be true but is no longer, but we keep it here so that
+  # Terraform doesn't delete and recreate the zone.
   description = "Automatically managed zone by kubernetes.io/external-dns"
   depends_on  = [google_project_service.dns]
 }
@@ -19,6 +21,8 @@ resource "google_dns_record_set" "www-entry" {
 }
 
 
+# This is a record, for example, for europe-west1.subdomain.example.com.
+# It's used to serve requests in a region closer to the user.
 resource "google_dns_record_set" "www-entry-ar" {
   for_each = var.domain == "" ? {} : var.additional_regions
 
