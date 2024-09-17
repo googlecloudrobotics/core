3cdaac04f86ac991d31a4ea5a086fc586953b91d
ensonic <ensonic@google.com>
2024-09-17 13:44:38 +0000

    Start to apply the layer 1 customization.
    * make the -robot bucket optional
    * make the robot-service account optional
    
    I wanted to also avoid the token-vendor rollout in base-cloud, but that
    is related to human-acl and oauth2-proxy usage.

--- src/bootstrap/cloud/terraform/gcs.tf
+++ src/bootstrap/cloud/terraform/gcs.tf
@@ -5,25 +5,29 @@ resource "google_storage_bucket" "robot" {
   uniform_bucket_level_access = "true"
   force_destroy               = "true"
   depends_on                  = [google_project_service.project-services["storage-component.googleapis.com"]]
+  count                       = var.onprem_federation ? 1 : 0
 }
 
 resource "google_storage_bucket_object" "setup_robot_image_reference" {
   name          = "setup_robot_image_reference.txt"
   content       = var.robot_image_reference
-  bucket        = google_storage_bucket.robot.name
+  bucket        = google_storage_bucket.robot[0].name
   cache_control = "private, max-age=0, no-transform"
+  count         = var.onprem_federation ? 1 : 0
 }
 
 resource "google_storage_bucket_object" "setup_robot_crc_version" {
   name          = "setup_robot_crc_version.txt"
   content       = var.crc_version
-  bucket        = google_storage_bucket.robot.name
+  bucket        = google_storage_bucket.robot[0].name
   cache_control = "private, max-age=0, no-transform"
+  count         = var.onprem_federation ? 1 : 0
 }
 
 resource "google_storage_bucket_object" "setup_robot" {
   name          = "setup_robot.sh"
   source        = "../../robot/setup_robot.sh"
-  bucket        = google_storage_bucket.robot.name
+  bucket        = google_storage_bucket.robot[0].name
   cache_control = "private, max-age=0, no-transform"
+  count         = var.onprem_federation ? 1 : 0
 }
