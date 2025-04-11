db99392d86f1cd84d55b4c75b65079e021c2df86
Rodrigo Queiro <rodrigoq@google.com>
2025-04-11 16:43:44 +0200

    Use informer to cache config maps in-memory. (#521)
    This should avoid outages due to the token-vendor hitting either the
    client-side or server-side rate limit when clients ask for tokens too
    frequently. I expect the next scaling limit will be the rate at which
    nginx can handle the requests or the token-vendor can do the
    cryptographic checks on the JWTs.
    
    Tested by deploying to my project and seeing if it falls over - also
    k8s.go has unit test coverage - I didn't have a good idea how to do load
    testing.

--- src/go/cmd/token-vendor/repository/k8s/k8s.go
+++ src/go/cmd/token-vendor/repository/k8s/k8s.go
@@ -18,21 +18,33 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"time"
 
 	"github.com/pkg/errors"
 
 	corev1 "k8s.io/api/core/v1"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
 
 	"github.com/googlecloudrobotics/core/src/go/cmd/token-vendor/repository"
+	"github.com/googlecloudrobotics/ilog"
 )
+
+// Re-list all ConfigMaps periodically. If this causes problems, consider
+// setting to 0 instead to disable, but hopefully this provides provides
+// some defense against bugs without being too costly.
+const resyncPeriod = 1 * time.Hour
 
 // K8sRepository uses Kubernetes configmaps as public key backend for devices.
 type K8sRepository struct {
 	kcl kubernetes.Interface // client-go Clientset
 	ns  string               // The namespace to use
+
+	cmInformer cache.SharedIndexInformer
 }
 
 // NewK8sRepository creates a new K8sRepository key repository.
@@ -41,7 +53,27 @@ type K8sRepository struct {
 // either a k8s.io/client-go/kubernetes/fake.NewSimpleClientset() for `kcl`
 // for testing, or a real Interface from kubernetes.NewForConfig(..).
 func NewK8sRepository(ctx context.Context, kcl kubernetes.Interface, ns string) (*K8sRepository, error) {
-	return &K8sRepository{kcl: kcl, ns: ns}, nil
+	// The informer provides an in-memory cache and prevents us from hammering the apiserver.
+	cmInformer := cache.NewSharedIndexInformer(
+		&cache.ListWatch{
+			ListFunc: func(options metav1.ListOptions) (object runtime.Object, e error) {
+				return kcl.CoreV1().ConfigMaps(ns).List(ctx, options)
+			},
+			WatchFunc: func(options metav1.ListOptions) (i watch.Interface, e error) {
+				return kcl.CoreV1().ConfigMaps(ns).Watch(ctx, options)
+			},
+		},
+		&corev1.ConfigMap{},
+		resyncPeriod,
+		cache.Indexers{},
+	)
+	go cmInformer.Run(ctx.Done())
+	// Wait for the cache to sync before returning so we don't serve requests
+	// until we're ready.
+	if !cache.WaitForCacheSync(ctx.Done(), cmInformer.HasSynced) {
+		return nil, fmt.Errorf("failed to sync configmap cache")
+	}
+	return &K8sRepository{kcl: kcl, ns: ns, cmInformer: cmInformer}, nil
 }
 
 const (
@@ -54,13 +86,13 @@ const (
 
 // ListAllDeviceIDs returns a slice of all device identifiers found in the namespace.
 func (k *K8sRepository) ListAllDeviceIDs(ctx context.Context) ([]string, error) {
-	ls, err := k.kcl.CoreV1().ConfigMaps(k.ns).List(ctx, metav1.ListOptions{})
-	if err != nil {
-		return []string{}, errors.Wrapf(err, "failed to list configmaps from namespace %q", k.ns)
-	}
+	objs := k.cmInformer.GetStore().List()
 	names := make([]string, 0)
-	for _, cm := range ls.Items {
-		names = append(names, cm.GetName())
+	for _, obj := range objs {
+		cm, ok := obj.(*corev1.ConfigMap)
+		if ok {
+			names = append(names, cm.GetName())
+		}
 	}
 	return names, nil
 }
@@ -71,13 +103,18 @@ func (k *K8sRepository) ListAllDeviceIDs(ctx context.Context) ([]string, error) 
 // error if the configmap is not found or is not valid.
 func (k *K8sRepository) LookupKey(ctx context.Context, deviceID string) (*repository.Key, error) {
 	slog.Debug("looking up public key", slog.String("Namespace", k.ns), slog.String("ConfigMap", deviceID))
-	cm, err := k.kcl.CoreV1().ConfigMaps(k.ns).Get(ctx, deviceID, metav1.GetOptions{})
+	obj, exists, err := k.cmInformer.GetStore().GetByKey(k.ns + "/" + deviceID)
 	if err != nil {
-		if kerrors.IsNotFound(err) {
-			return nil, errors.Wrapf(repository.ErrNotFound, "failed to retrieve configmap %q/%q", k.ns, deviceID)
-		}
-		return nil, errors.Wrapf(err, "failed to retrieve configmap %q/%q", k.ns, deviceID)
+		return nil, errors.Wrapf(err, "failed to retrieve configmap %q/%q from cache", k.ns, deviceID)
 	}
+	if !exists {
+		return nil, errors.Wrapf(repository.ErrNotFound, "failed to retrieve configmap %q/%q", k.ns, deviceID)
+	}
+	cm, ok := obj.(*corev1.ConfigMap)
+	if !ok {
+		return nil, fmt.Errorf("unexpected object type: %T", obj)
+	}
+
 	key, found := cm.Data[pubKey]
 	if !found {
 		return nil, fmt.Errorf("configmap %q/%q does not contain key %q", k.ns, deviceID, pubKey)
@@ -93,25 +130,31 @@ func (k *K8sRepository) LookupKey(ctx context.Context, deviceID string) (*reposi
 // already the public key section of the configmap is updated.
 func (k *K8sRepository) PublishKey(ctx context.Context, deviceID, publicKey string) error {
 	slog.Debug("publishing key", slog.String("DeviceID", deviceID))
-	cm, err := createPubKeyDeviceConfig(deviceID, publicKey)
+	cm, err := createPubKeyDeviceConfig(deviceID, k.ns, publicKey)
 	if err != nil {
 		return errors.Wrapf(err, "failed to init device configmap %q/%q", k.ns, deviceID)
 	}
-	if _, err = k.kcl.CoreV1().ConfigMaps(k.ns).Create(ctx, cm, metav1.CreateOptions{}); err == nil { // no error
-
-		slog.Info("created new configmap", slog.String("Namespace", k.ns), slog.String("ConfigMap", deviceID))
+	_, err = k.kcl.CoreV1().ConfigMaps(k.ns).Create(ctx, cm, metav1.CreateOptions{})
+	if err == nil { // no error
+		// Add to the informer store so that LookupKey can be used immediately.
+		if err := k.cmInformer.GetStore().Add(cm); err != nil {
+			slog.Warn("failed to add to informer store", slog.String("DeviceID", deviceID), ilog.Err(err))
+		}
 		return nil
 	}
 	if !kerrors.IsAlreadyExists(err) { // any error not AlreadyExist
 		return errors.Wrapf(err, "failed to create device configmap %q/%q", k.ns, deviceID)
 	}
-	// AlreadyExist error, updating configmap
+	// AlreadyExist error, updating configmap.
 	// We do not want to override any other keys besides the public key here.
 	// createPubKeyDeviceConfig only creates a minimum configmap so updating is safe here.
-	if _, err = k.kcl.CoreV1().ConfigMaps(k.ns).Update(ctx, cm, metav1.UpdateOptions{}); err != nil {
+	if _, err := k.kcl.CoreV1().ConfigMaps(k.ns).Update(ctx, cm, metav1.UpdateOptions{}); err != nil {
 		return errors.Wrapf(err, "configmap %q/%q exists but failed to update it", k.ns, deviceID)
 	}
-	slog.Info("updated configmap with new public key", slog.String("Namespace", k.ns), slog.String("ConfigMap", deviceID))
+	// Update the informer store so that LookupKey can be used immediately.
+	if err := k.cmInformer.GetStore().Update(cm); err != nil {
+		slog.Warn("failed to update informer store", slog.String("DeviceID", deviceID), ilog.Err(err))
+	}
 	return nil
 }
 
@@ -119,14 +162,15 @@ func (k *K8sRepository) PublishKey(ctx context.Context, deviceID, publicKey stri
 //
 // This is used also during update of existing devices. Make sure no default values
 // are used here which could override a manually set key.
-func createPubKeyDeviceConfig(name string, pk string) (*corev1.ConfigMap, error) {
+func createPubKeyDeviceConfig(name, namespace, pk string) (*corev1.ConfigMap, error) {
 	return &corev1.ConfigMap{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "ConfigMap",
 			APIVersion: "v1",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
+			Namespace: namespace,
+			Name:      name,
 			Labels: map[string]string{
 				"app.kubernetes.io/managed-by": "token-vendor",
 			},
