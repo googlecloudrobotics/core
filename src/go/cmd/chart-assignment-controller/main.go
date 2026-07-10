googlecloudrobotics/core

commit 67d18a2f82e92120285dbddcf85fd2816699d9d2

Bump kustomize, run gomod.sh and add changes to support it (#266)
I need all dependencies (including transitive ones) to use
`k8s.io/client-go` v0.27+

The old version of kustomize/api uses v0.20.4:

https://github.com/kubernetes-sigs/kustomize/blob/api/v0.13.4/cmd/depprobcheck/go.mod#L57

Bumping it makes it use v0.27.0:

https://github.com/kubernetes-sigs/kustomize/blob/api/v0.15.0/cmd/depprobcheck/go.mod#L59

I really really hope this is the last one

I also realize I didn't run `gomod.sh` in the previous PR. So I ran it
here.

---------

Signed-off-by: methylDragon <methylDragon@google.com>

src/go/cmd/chart-assignment-controller/main.go
@@ -36,6 +36,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
 var (
@@ -105,8 +107,8 @@ func runController(ctx context.Context, cfg *rest.Config, cluster string) error 
 
 	mgr, err := manager.New(cfg, manager.Options{
 		Scheme:                 sc,
-		Port:                   *webhookPort,
-		MetricsBindAddress:     "0", // disabled
+		WebhookServer:          webhook.NewServer(webhook.Options{CertDir: *certDir, Port: *webhookPort}),
+		Metrics:                metricsserver.Options{BindAddress: "0"}, // disabled
 		HealthProbeBindAddress: ":8080",
 	})
 	if err != nil {
@@ -120,7 +122,6 @@ func runController(ctx context.Context, cfg *rest.Config, cluster string) error 
 	if *webhookEnabled {
 		webhook := chartassignment.NewValidationWebhook(mgr)
 		srv := mgr.GetWebhookServer()
-		srv.CertDir = *certDir
 		srv.Register("/chartassignment/validate", webhook)
 	}
 

src/go/cmd/app-rollout-controller/main.go
@@ -35,6 +35,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
 var (
@@ -76,8 +78,8 @@ func runController(ctx context.Context, cfg *rest.Config, params map[string]inte
 
 	mgr, err := manager.New(cfg, manager.Options{
 		Scheme:                 sc,
-		Port:                   *webhookPort,
-		MetricsBindAddress:     "0", // disabled
+		WebhookServer:          webhook.NewServer(webhook.Options{CertDir: *certDir, Port: *webhookPort}),
+		Metrics:                metricsserver.Options{BindAddress: "0"}, // disabled
 		HealthProbeBindAddress: ":8080",
 	})
 	if err != nil {
@@ -89,7 +91,6 @@ func runController(ctx context.Context, cfg *rest.Config, params map[string]inte
 	mgr.AddHealthzCheck("trivial", func(_ *http.Request) error { return nil })
 
 	srv := mgr.GetWebhookServer()
-	srv.CertDir = *certDir
 
 	srv.Register("/approllout/validate", approllout.NewAppRolloutValidationWebhook(mgr))
 	srv.Register("/app/validate", approllout.NewAppValidationWebhook(mgr))

src/go/cmd/setup-dev/main.go
@@ -38,7 +38,6 @@ import (
 	"golang.org/x/oauth2/google"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"k8s.io/client-go/tools/clientcmd"
 	clientapi "k8s.io/client-go/tools/clientcmd/api"
 )

src/go.mod
@@ -64,6 +64,7 @@ require (
 	github.com/google/go-cmp v0.6.0
 	github.com/googlecloudrobotics/ilog v0.0.0-20231123101900-2533d3d7f8e1
 	github.com/sirupsen/logrus v1.9.0
+	k8s.io/klog/v2 v2.110.1
 )
 
 require (
@@ -92,7 +93,7 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/google/btree v1.1.2 // indirect
-	github.com/google/gnostic-models v0.6.8 // indirect
+	github.com/google/gnostic-models v0.6.9-0.20230804172637-c7be7c783f49 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
 	github.com/google/safetext v0.0.0-20221026122733-23539d61753f // indirect
@@ -134,15 +135,15 @@ require (
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20231127180814-3a041ad873d4 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231127180814-3a041ad873d4 // indirect
+	gopkg.in/evanphx/json-patch.v5 v5.6.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/component-base v0.28.4 // indirect
-	k8s.io/klog/v2 v2.110.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20231129212854-f0671cc7e66a // indirect
 	k8s.io/utils v0.0.0-20231127182322-b307cd553661 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
-	sigs.k8s.io/kustomize/api v0.13.5-0.20230601165947-6ce0bf390ce3 // indirect
-	sigs.k8s.io/kustomize/kyaml v0.14.3-0.20230601165947-6ce0bf390ce3 // indirect
+	sigs.k8s.io/kustomize/api v0.15.0 // indirect
+	sigs.k8s.io/kustomize/kyaml v0.15.0 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
 )

src/go.sum
@@ -198,8 +198,8 @@ github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c/go.mod h1:lNA+9X1NB3Z
 github.com/google/btree v1.0.0/go.mod h1:lNA+9X1NB3Zf8V7Ke586lFgjr2dZNuvo3lPJSGZ5JPQ=
 github.com/google/btree v1.1.2 h1:xf4v41cLI2Z6FxbKm+8Bu+m8ifhj15JuZ9sa0jZCMUU=
 github.com/google/btree v1.1.2/go.mod h1:qOPhT0dTNdNzV6Z/lhRX0YXUafgPLFUh+gZMl761Gm4=
-github.com/google/gnostic-models v0.6.8 h1:yo/ABAfM5IMRsS1VnXjTBvUb61tFIHozhlYvRgGre9I=
-github.com/google/gnostic-models v0.6.8/go.mod h1:5n7qKqH0f5wFt+aWF8CW6pZLLNOfYuF5OpfBSENuI8U=
+github.com/google/gnostic-models v0.6.9-0.20230804172637-c7be7c783f49 h1:0VpGH+cDhbDtdcweoyCVsF3fhN8kejK6rFe/2FFX2nU=
+github.com/google/gnostic-models v0.6.9-0.20230804172637-c7be7c783f49/go.mod h1:BkkQ4L1KS1xMt2aWSPStnn55ChGC0DPOn2FQYj+f25M=
 github.com/google/go-cmp v0.2.0/go.mod h1:oXzfMopK8JAjlY9xF4vHSVASa0yLyX7SntLO5aqRK0M=
 github.com/google/go-cmp v0.3.0/go.mod h1:8QqcDgzrUqlUb/G2PQTWiueGozuR1884gddMywk6iLU=
 github.com/google/go-cmp v0.3.1/go.mod h1:8QqcDgzrUqlUb/G2PQTWiueGozuR1884gddMywk6iLU=
@@ -776,6 +776,8 @@ gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b/go.mod h1:Co6ibVJAznAaIkqp8
 gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c h1:Hei/4ADfdWqJk1ZMxUNpqntNwaWcugrBjAiHlqqRiVk=
 gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c/go.mod h1:JHkPIbrfpd72SG/EVd6muEfDQjcINNoR0C8j2r3qZ4Q=
 gopkg.in/errgo.v2 v2.1.0/go.mod h1:hNsd1EY+bozCKY1Ytp96fpM3vjJbqLJn88ws8XvfDNI=
+gopkg.in/evanphx/json-patch.v5 v5.6.0 h1:BMT6KIwBD9CaU91PJCZIe46bDmBWa9ynTQgJIOpfQBk=
+gopkg.in/evanphx/json-patch.v5 v5.6.0/go.mod h1:/kvTRh1TVm5wuM6OkHxqXtE/1nUZZpihg29RtuIyfvk=
 gopkg.in/h2non/gock.v1 v1.1.2 h1:jBbHXgGBK/AoPVfJh5x4r/WxIrElvbLel8TCZkkZJoY=
 gopkg.in/h2non/gock.v1 v1.1.2/go.mod h1:n7UGz/ckNChHiK05rDoiC4MYSunEC/lyaUm2WWaDva0=
 gopkg.in/inf.v0 v0.9.1 h1:73M5CoZyi3ZLMOyDlQh031Cx6N9NDJ2Vvfl76EDAgDc=
@@ -829,10 +831,10 @@ sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd h1:EDPBXCAspyGV4jQlpZSudPeMm
 sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd/go.mod h1:B8JuhiUyNFVKdsE8h686QcCxMaH6HrOAZj4vswFpcB0=
 sigs.k8s.io/kind v0.17.0 h1:CScmGz/wX66puA06Gj8OZb76Wmk7JIjgWf5JDvY7msM=
 sigs.k8s.io/kind v0.17.0/go.mod h1:Qqp8AiwOlMZmJWs37Hgs31xcbiYXjtXlRBSftcnZXQk=
-sigs.k8s.io/kustomize/api v0.13.5-0.20230601165947-6ce0bf390ce3 h1:XX3Ajgzov2RKUdc5jW3t5jwY7Bo7dcRm+tFxT+NfgY0=
-sigs.k8s.io/kustomize/api v0.13.5-0.20230601165947-6ce0bf390ce3/go.mod h1:9n16EZKMhXBNSiUC5kSdFQJkdH3zbxS/JoO619G1VAY=
-sigs.k8s.io/kustomize/kyaml v0.14.3-0.20230601165947-6ce0bf390ce3 h1:W6cLQc5pnqM7vh3b7HvGNfXrJ/xL6BDMS0v1V/HHg5U=
-sigs.k8s.io/kustomize/kyaml v0.14.3-0.20230601165947-6ce0bf390ce3/go.mod h1:JWP1Fj0VWGHyw3YUPjXSQnRnrwezrZSrApfX5S0nIag=
+sigs.k8s.io/kustomize/api v0.15.0 h1:6Ca88kEOBVotHDw+y2IsIMYtg9Pvv7MKpW9JMyF/OH4=
+sigs.k8s.io/kustomize/api v0.15.0/go.mod h1:p19kb+E14gN7zcIBR/nhByJDAfUa7N8mp6ZdH/mMXbg=
+sigs.k8s.io/kustomize/kyaml v0.15.0 h1:ynlLMAxDhrY9otSg5GYE2TcIz31XkGZ2Pkj7SdolD84=
+sigs.k8s.io/kustomize/kyaml v0.15.0/go.mod h1:+uMkBahdU1KNOj78Uta4rrXH+iH7wvg+nW7+GULvREA=
 sigs.k8s.io/structured-merge-diff/v4 v4.4.1 h1:150L+0vs/8DA78h1u02ooW1/fFq/Lwr+sGiqlzvrtq4=
 sigs.k8s.io/structured-merge-diff/v4 v4.4.1/go.mod h1:N8hJocpFajUSSeSJ9bOZ77VzejKZaXsTtZo4/u7Io08=
 sigs.k8s.io/yaml v1.3.0/go.mod h1:GeOyir5tyXNByN85N/dRIT9es5UQNerPYEKK56eTBm8=

src/go/cmd/app-rollout-controller/BUILD.bazel
@@ -23,6 +23,8 @@ go_library(
         "@io_k8s_sigs_controller_runtime//pkg/log/zap:go_default_library",
         "@io_k8s_sigs_controller_runtime//pkg/manager:go_default_library",
         "@io_k8s_sigs_controller_runtime//pkg/manager/signals:go_default_library",
+        "@io_k8s_sigs_controller_runtime//pkg/metrics/server:go_default_library",
+        "@io_k8s_sigs_controller_runtime//pkg/webhook:go_default_library",
     ],
 )
 

src/go/cmd/chart-assignment-controller/BUILD.bazel
@@ -18,6 +18,8 @@ go_library(
         "@io_k8s_sigs_controller_runtime//pkg/log/zap:go_default_library",
         "@io_k8s_sigs_controller_runtime//pkg/manager:go_default_library",
         "@io_k8s_sigs_controller_runtime//pkg/manager/signals:go_default_library",
+        "@io_k8s_sigs_controller_runtime//pkg/metrics/server:go_default_library",
+        "@io_k8s_sigs_controller_runtime//pkg/webhook:go_default_library",
         "@io_opencensus_go//trace:go_default_library",
         "@io_opencensus_go_contrib_exporter_stackdriver//:go_default_library",
     ],

src/go/cmd/setup-dev/BUILD.bazel
@@ -14,7 +14,6 @@ go_library(
         "@com_github_cenkalti_backoff//:go_default_library",
         "@io_k8s_apimachinery//pkg/runtime/schema:go_default_library",
         "@io_k8s_client_go//dynamic:go_default_library",
-        "@io_k8s_client_go//plugin/pkg/client/auth/gcp:go_default_library",
         "@io_k8s_client_go//tools/clientcmd:go_default_library",
         "@io_k8s_client_go//tools/clientcmd/api:go_default_library",
         "@org_golang_x_oauth2//:go_default_library",

commit 0a475966ab9b886c5b0e05ef3ff7308d3627ab3a

Make chart-assignment-controller webhook certs optional (#799)
This allows deploying the controller on vanilla k3s clusters without a
hard dependency on cert-manager for TLS certificates. I think this might
have been broken in
https://github.com/googlecloudrobotics/core/commit/67d18a2f82e92120285dbddcf85fd2816699d9d2.

src/go/cmd/chart-assignment-controller/main.go
@@ -107,19 +107,15 @@ func runController(ctx context.Context, cfg *rest.Config, cluster string) error 
 	scheme.AddToScheme(sc)
 	apps.AddToScheme(sc)
 
-	webhookOptions := webhook.Options{
-		Port: *webhookPort,
-	}
-	if *certDir != "" {
-		webhookOptions.CertDir = *certDir
-	}
-
-	mgr, err := manager.New(cfg, manager.Options{
+	opts := manager.Options{
 		Scheme:                 sc,
-		WebhookServer:          webhook.NewServer(webhookOptions),
 		Metrics:                metricsserver.Options{BindAddress: "0"}, // disabled
 		HealthProbeBindAddress: fmt.Sprintf(":%d", *healthzPort),
-	})
+	}
+	if *webhookEnabled {
+		opts.WebhookServer = webhook.NewServer(webhook.Options{CertDir: *certDir, Port: *webhookPort})
+	}
+	mgr, err := manager.New(cfg, opts)
 	if err != nil {
 		return fmt.Errorf("create controller manager: %w", err)
 	}