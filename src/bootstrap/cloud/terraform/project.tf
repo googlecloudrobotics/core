fb3c674276fa8721c1e683854e7ca3bbdfa8d071
Teo Koon Peng <koonpeng@google.com>
2026-07-29 10:14:10 +0800

    Ensure that all services are enabled before provisioning resources (#829)

--- src/bootstrap/cloud/terraform/project.tf
+++ src/bootstrap/cloud/terraform/project.tf
@@ -9,6 +9,10 @@ removed {
 
 data "google_project" "project" {
   project_id = var.id
+
+  # Depending on `google_project_service.project-services` makes any resource that references
+  # this wait for services to be enabled.
+  depends_on = [google_project_service.project-services]
 }
 
 resource "google_project_iam_member" "owner_group" {
@@ -21,7 +25,7 @@ resource "google_project_iam_member" "owner_group" {
 # We can't use google_project_services because Endpoints adds services
 # dynamically.
 resource "google_project_service" "project-services" {
-  project            = data.google_project.project.project_id
+  project            = var.id
   disable_on_destroy = false
   for_each = toset(concat([
     "artifactregistry.googleapis.com",
