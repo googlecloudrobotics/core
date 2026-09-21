5f3782ed97b072a80c728ac19a522193cc8d4c47
Stefan Sauer <ensonic@google.com>
2026-09-21 08:23:46 +0200

    synk: generalize resource grouping to install RBAC before other resources (#913)
    Make resource partitioning in applyAll generic so that resources can be
    separated into three groups: CustomResourceDefinitions, RBAC-related
    resources (Roles, RoleBindings, ClusterRoles, ClusterRoleBindings, and
    ServiceAccounts), and remaining resources.
    
    Apply the groups in sequence to ensure CRDs and RBAC permissions are
    established before subsequent workloads are installed.
    
    TAG=agy
    CONV=d1d5e845-2765-44b2-ab59-1fa15cb5878d

--- src/go/pkg/synk/sort_test.go
+++ src/go/pkg/synk/sort_test.go
@@ -15,9 +15,11 @@
 package synk
 
 import (
+	"reflect"
 	"testing"
 
 	apps "github.com/googlecloudrobotics/core/src/go/pkg/apis/apps/v1alpha1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 func TestLessResourceSetStatusGroup(t *testing.T) {
@@ -80,6 +82,11 @@ func TestLess(t *testing.T) {
 		{newGvknn("g", "v", "k", "ns", "a"), newGvknn("g", "v", "k", "ns", "b")},
 		{newGvknn("g", "v", "ServiceAccount", "ns", "a"), newGvknn("g", "v", "Secret", "ns", "b")},
 		{newGvknn("g", "v", "Secret", "ns", "a"), newGvknn("g", "v", "ServiceAccount2", "ns", "b")},
+		{newGvknn("v1", "v", "Secret", "ns", "a"), newGvknn("rbac.authorization.k8s.io", "v1", "Role", "ns", "b")},
+		{newGvknn("rbac.authorization.k8s.io", "v1", "ClusterRole", "", "a"), newGvknn("rbac.authorization.k8s.io", "v1", "ClusterRoleBinding", "", "b")},
+		{newGvknn("rbac.authorization.k8s.io", "v1", "Role", "ns", "a"), newGvknn("rbac.authorization.k8s.io", "v1", "RoleBinding", "ns", "b")},
+		{newGvknn("rbac.authorization.k8s.io", "v1", "RoleBinding", "ns", "a"), newGvknn("", "v1", "ConfigMap", "ns", "b")},
+		{newGvknn("rbac.authorization.k8s.io", "v1", "ClusterRoleBinding", "", "a"), newGvknn("apps", "v1", "Deployment", "ns", "b")},
 	} {
 		if !less(tc.a, tc.b) {
 			t.Errorf("expected a (%v) < b (%v)", tc.a, tc.b)
@@ -87,5 +94,29 @@ func TestLess(t *testing.T) {
 		if less(tc.b, tc.a) {
 			t.Errorf("expected b (%v) >= a (%v)", tc.b, tc.a)
 		}
+	}
+}
+
+func TestSortResources_RBACOrder(t *testing.T) {
+	ns := newUnstructured("v1", "Namespace", "", "ns1")
+	sa := newUnstructured("v1", "ServiceAccount", "ns1", "sa1")
+	secret := newUnstructured("v1", "Secret", "ns1", "secret1")
+	clusterRole := newUnstructured("rbac.authorization.k8s.io/v1", "ClusterRole", "", "cr1")
+	role := newUnstructured("rbac.authorization.k8s.io/v1", "Role", "ns1", "role1")
+	clusterRoleBinding := newUnstructured("rbac.authorization.k8s.io/v1", "ClusterRoleBinding", "", "crb1")
+	roleBinding := newUnstructured("rbac.authorization.k8s.io/v1", "RoleBinding", "ns1", "rb1")
+	cm := newUnstructured("v1", "ConfigMap", "ns1", "cm1")
+	deploy := newUnstructured("apps/v1", "Deployment", "ns1", "deploy1")
+
+	resources := []*unstructured.Unstructured{
+		deploy, roleBinding, cm, role, clusterRoleBinding, secret, clusterRole, sa, ns,
+	}
+	sortResources(resources)
+
+	want := []*unstructured.Unstructured{
+		ns, sa, secret, clusterRole, role, clusterRoleBinding, roleBinding, cm, deploy,
+	}
+	if !reflect.DeepEqual(resources, want) {
+		t.Errorf("sortResources() mismatch:\ngot:  %v\nwant: %v", resources, want)
 	}
 }
